e76aba8db4d442c3a3dcc802608a99b308c4c295
Prithvi Raj <135632150+amprithviraj@users.noreply.github.com>
2023-11-27 20:36:33 +0530

    Add files via upload

---
+++ Data Science Ecosystem .py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Data Science Tools and Ecosystem
+# 
+
+# # Intoduction
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# # Data Science Languages
+# Some of the popular languages that Data Scientists use are:
+# 1. **Python:** Widely used for data analysis, machine learning, and scientific computing
+# 2. **R:** Specifically designed for statistics and data analysis
+# 3. **SQL:** Essential for database querying and manipulation
+
+# # Data science libraries
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. **Pandas:** A powerful library for data manipulation and analysis in Python.
+# 2. **NumPy:** Essential for numerical computing and working with large, multi-dimensional arrays and matrices.
+# 3. **Scikit-learn:** A machine learning library for classical machine learning algorithms and tools.
+
+# # Data Science Tools
+# 
+# | Development Environment |
+# |------------------------|
+# | Jupyter Notebook       |
+# | RStudio                |
+# | VSCode (Visual Studio Code) |
+
+# # Arithmetic expression examples
+# 
+# 
+# Introduction text goes here...
+# 
+# ### Examples of Evaluating Arithmetic Expressions in Python
+# 
+# Below are a few examples of evaluating arithmetic expressions in Python:
+# 
+# 1. Addition:
+#    ```python
+#    result = 5 + 3
+#    print(result)  # Output: 8
+# 
+
+# In[17]:
+
+
+# This is a simple arithmetic expression to multiply then add integers.
+result = (3 * 4) + 5
+print(result)
+
+
+# In[18]:
+
+
+# This will convert 200 minutes to hours by dividing by 60.
+minutes = 200
+hours = minutes / 60
+print(hours)
+
+
+# # Introduction to Data Science
+# 
+# Introduction text goes here...
+# 
+# ## Objectives:
+# 
+# - **List popular languages for Data Science.**
+# - **Introduce key tools and libraries used in Data Science.**
+# - **Demonstrate creating and executing code cells in Jupyter Notebook.**
+# - **Explore common tasks in data analysis and manipulation.**
+# - **Illustrate best practices for Markdown formatting in notebooks.**
+# 
+
+# # Author
+# 
+# PRITHVI RAJ
+
+# In[ ]:
+
+
+
+
